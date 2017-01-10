481c9ec86a892b661455b3d07b1acbc9df97c489
Jonathon Gaff <jonathongaff@anlextwls025-163.wl.anl-external.org>
2017-01-10 12:57:34 -0600

    Functional ingester
    Ingested tested to work properly with OQMD dataset and converter
    (commit of converter coming soon).

--- prototypes/ingester_prototype.py
+++ prototypes/ingester_prototype.py
@@ -2,44 +2,44 @@ import globus_auth
 from pickle import load
 from sys import exit
 
-default_filename = "oqmd_all_v2.pickle"
-
-verbose = True
-ingest_limit = 1
-datacite_namespace = "https://schema.labs.datacite.org/meta/kernel-4.0/metadata.xsd"
-dc_namespace = "http://dublincore.org/documents/dcmi-terms"
+#datacite_namespace = "https://schema.labs.datacite.org/meta/kernel-4.0/metadata.xsd"
+#dc_namespace = "http://dublincore.org/documents/dcmi-terms"
 
 
 #Formats a single gmeta entry from the data dictionary
 #With full=false, the returned entry can be added to a gmeta list, but cannot be ingested directly
 #With full=true, the returned entry is ready for ingestion, but not inclusion in a gmeta list
 def format_single_gmeta(data_dict, full=False):
+	if not data_dict.get("data", None):
+		print "Error: No data in entry"
+		return None
 	if not data_dict.get("context", None):
 		data_dict["context"] = {}
-	if not data_dict.get("context", {}).get("datacite", None):
-		data_dict["context"]["datacite"] = datacite_namespace
-	if not data_dict.get("context", {}).get("dc", None):
-		data_dict["context"]["dc"] = dc_namespace
+#	if not data_dict.get("context", {}).get("datacite", None):
+#		data_dict["context"]["datacite"] = datacite_namespace
+#	if not data_dict.get("context", {}).get("dc", None):
+#		data_dict["context"]["dc"] = dc_namespace
 	if not data_dict.get("globus_subject", None):
 		data_dict["globus_subject"] = ""
 	if not data_dict.get("globus_id", None):
 		data_dict["globus_id"] = ""
-	if data_dict.get("data_context", None):
-		for elem in data_dict.get("data", {}).keys():
-			elem = data_dict["data_context"] + elem
+	if not data_dict.get("globus_source", None):
+		data_dict["globus_source"] = ""
+#	if data_dict.get("data_context", None):
+#		for elem in data_dict.get("data", {}).keys():
+#			elem = data_dict["data_context"] + ":" + elem
 	gmeta = {
 		"@datatype":"GMetaEntry",
 		"@version":"2016-11-09",
 		"subject":data_dict["globus_subject"],
 		"visible_to":["public"],
 		"id":data_dict["globus_id"],
-		"content":data_dict.get("data", {})
+		"source_id":data_dict["globus_source"],
+		"content":data_dict["data"]
 		}
 	gmeta["content"]["@context"] = data_dict["context"]
 
 	if full:
-		if not data_dict.get("globus_source"):
-			data_dict["globus_source"] = ""
 		gmeta_full = {
 			"@datatype":"GIngest",
 			"@version":"2016-11-09",
@@ -54,12 +54,12 @@ def format_single_gmeta(data_dict, full=False):
 		return gmeta_full
 
 #Formats a list of gmeta entries into a GMetaList, suitable for ingesting
-def format_multi_gmeta(data_list, globus_source=""):
+def format_multi_gmeta(data_list):
 	gmeta = {
 		"@datatype":"GIngest",
 		"@version":"2016-11-09",
 		"ingest_type":"GMetaList",
-		"source_id":globus_source,
+		"source_id":data_list[0]["source_id"],
 		"ingest_data":{
 			"@datatype":"GMetaList",
 			"@version":"2016-11-09",
@@ -69,7 +69,7 @@ def format_multi_gmeta(data_list, globus_source=""):
 	return gmeta
 
 #Takes a pickled list of dicts and ingests into Globus Search
-def ingest_pickle(pickle_filename):
+def ingest_pickle(pickle_filename, ingest_limit=0, verbose=True):
 	#Authentication
 	if verbose:
 		print "Authenticating"
@@ -104,7 +104,7 @@ def ingest_pickle(pickle_filename):
 			single_ingestable = format_single_gmeta(record, full=False)
 			list_ingestable.append(single_ingestable)
 			count += 1
-			if count >= ingest_limit:
+			if ingest_limit != 0 and count >= ingest_limit:
 				break
 		multi_ingestable = format_multi_gmeta(list_ingestable)
 		client.ingest(multi_ingestable)
@@ -114,6 +114,8 @@ def ingest_pickle(pickle_filename):
 		print "Success"
 
 if __name__ == "__main__":
-	print "Using data from " + default_filename + ":\n"
-	ingest_pickle(default_filename)
+	default_filename = "oqmd_all_v2.pickle"
+	default_ingest_limit = 1000
+	print "Using " + str(default_ingest_limit) + " records from " + default_filename + ":\n"
+	ingest_pickle(default_filename, ingest_limit=default_ingest_limit)
 
