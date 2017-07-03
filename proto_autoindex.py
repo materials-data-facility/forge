b6b521798fb7482c601236561cc9de41bf22acc8
jgaff <jgaff@uchicago.edu>
2017-07-03 12:28:11 -0500

    Autoindexer checkpoint

--- proto_autoindex.py
+++ proto_autoindex.py
@@ -1,9 +1,15 @@
 import os
 import sys
+import json
+import argparse
+import random
 from importlib import import_module
 
+DEFAULT_GEN_CONV = "metadata_only"
 
-def autoindex(dataset_metadata, data_path, converter_dir="mdf_indexers/converters", default_converter="metadata_only", verbose=False):
+
+def autoindex(dataset_metadata, data_path, submit_feedstock=True, review_feedstock=True, default_converter=DEFAULT_GEN_CONV, verbose=False):
+    converter_dir = "mdf_indexers/converters"
     if verbose:
         print("Indexing dataset")
     if type(dataset_metadata) is dict:
@@ -52,7 +58,7 @@ def autoindex(dataset_metadata, data_path, converter_dir="mdf_indexers/converter
     else:
         converter_path = os.path.join(converter_dir, selected_converter + "_converter").strip(".")
         # Transform: '../part/of/path' => '..part.of.path'
-        converter = import_module(converter_path.replace("..", ".").replace("/", ".")
+        converter = import_module(converter_path.replace("..", ".").replace("/", "."))
 
     if verbose:
         print("Converting dataset")
@@ -62,7 +68,28 @@ def autoindex(dataset_metadata, data_path, converter_dir="mdf_indexers/converter
     if verbose:
         print("Dataset converted")
 
+    feedstock_path = 
+
+    if review_feedstock:
+        print("Please review the following to ensure that the dataset was converted correctly:")
+        
 
 
+def cli():
+    parser = argparse.ArgumentParser(description="This tool will automatically index a dataset, given the metadata and data location, and submit it to MDF.")
+    parser.add_argument("-m", "--md", "--metadata-path", help="The path to the metadata file", required=True)
+    parser.add_argument("-d", "--data-path", help="The path to the data", required=True)
+    submit_group = parser.add_mutually_exclusive_group()
+    submit_group.add_argument("-f", "--no-submit", "--feedstock-only", "--local", action="store_true", help="Do not submit the feedstock to MDF; only save it locally")
+    submit_group.add_argument("-s", "--auto-submit", "--autosubmit", "--no-check", "--no-review", action="store_true", help="Submit the feedstock to MDF without reviewing it first")
+    parser.add_argument("--default-converter", help="The converter to use if no specific alternative is found", default=DEFAULT_GEN_CONV)
+    parser.add_argument("-v", "--verbose", action="store_true", help="Print status messages")
+    args = parser.parse_args()
+
+    autoindex(dataset_metadata=parser.md, data_path=parser.data_path, submit_feedstock=not parser.no_submit, review_feedstock=not parser.auto_submit, default_converter=parser.default_converter, verbose=parser.verbose)
+
+
+if __name__ == "__main__":
+    cli()
 
 
