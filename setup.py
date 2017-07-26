f38447b959628ff3330d87af7ed28860edd4bb05
Ben Blaiszik <blaiszik@uchicago.edu>
2017-07-26 15:41:36 -0500

    initial setup.py -- will edit soon

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='mdf',
+    version='0.0.1',
+    packages=['mdf_dataworks'],
+    description='Materials Data Facility python package',
+    install_requires=[]
+)
