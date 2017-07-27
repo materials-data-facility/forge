e9a06c5d91aa7c318c6b806f43e9653d7c0b8567
Ben Blaiszik <blaiszik@uchicago.edu>
2017-07-27 11:01:07 -0500

    Initial package setup

--- setup.py
+++ setup.py
@@ -5,5 +5,15 @@ setup(
     version='0.0.1',
     packages=['mdf_dataworks'],
     description='Materials Data Facility python package',
-    install_requires=[]
+    install_requires=[
+        "globus-sdk>=1.1.1",
+        "requests>=2.18.1",
+        "tqdm>=4.14.0",
+        "ase>=3.14.1",
+        "beautifulsoup4>=4.6.0",
+        "jsonschema>=2.6.0",
+        "pymatgen>=2017.6.8",
+        "pymongo>=3.4.0",
+        "xmltodict>=0.10.2"
+    ]
 )
