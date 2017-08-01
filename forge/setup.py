a9f7f70bc8e7fe80d345fc3229efee56943944c6
jgaff <jgaff@uchicago.edu>
2017-08-01 09:12:15 -0500

    Split Forge and Refinery into separate packages
    Separating the mdf_forge and mdf_refinery into different packages.
    Forge remains the primary package for accessing data in the MDF, but
    Refinery will be the optional (and somewhat heavy) package for dataset
    creation. Refinery requires Forge for the toolbox. Also add the build
    and distilled dirs to .gitignore.

---
+++ forge/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mdf_forge',
+    version='0.0.1',
+    packages=['mdf_forge'],
+    description='Materials Data Facility python package',
+    install_requires=[
+        "globus-sdk>=1.1.1",
+        "requests>=2.18.1",
+        "tqdm>=4.14.0"
+    ]
+)
--- refinery/setup.py
+++ refinery/setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='mdf',
+    name='mdf_refinery',
     version='0.0.1',
-    packages=['mdf_dataworks'],
+    packages=find_packages(),
     description='Materials Data Facility python package',
     install_requires=[
+        "mdf_forge>=0.0.1",
         "globus-sdk>=1.1.1",
         "requests>=2.18.1",
         "tqdm>=4.14.0",
         "ase>=3.14.1",
         "beautifulsoup4>=4.6.0",
-        "jsonschema>=2.6.0",
+        "xmltodict>=0.10.2",
         "pymatgen>=2017.6.8",
-        "pymongo>=3.4.0",
-        "xmltodict>=0.10.2"
+        "jsonschema>=2.6.0",
+        "pymongo>=3.4.0"  # For bson.ObjectId
     ]
 )
