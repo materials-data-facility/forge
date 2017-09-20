5ca005cae319badc21b43a801bac7581f2a5fefa
jgaff <jgaff@uchicago.edu>
2017-09-20 16:06:55 -0500

    Specify minimum Python versions

--- forge/setup.py
+++ forge/setup.py
@@ -12,6 +12,7 @@ setup(
         "tqdm>=4.14.0",
         "six>=1.10.0"
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
