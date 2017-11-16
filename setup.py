b949336ee6921b58ff82512039c79f076e82b4f5
jgaff <jgaff@uchicago.edu>
2017-11-16 09:31:02 -0600

    Lower required six version
    Travis is having issues installing six 1.11.0 over 1.10.0.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         "globus-sdk>=1.2.1",
         "requests>=2.18.4",
         "tqdm>=4.19.4",
-        "six>=1.11.0"
+        "six>=1.10.0"
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
     classifiers=[
