983129497b5b6740bb2982dc3f0a8691c7cfc259
Logan Ward <ward.logan.t@gmail.com>
2018-04-20 14:36:50 -0500

    Added utility operation for getting dataset version

--- mdf_forge/forge.py
+++ mdf_forge/forge.py
@@ -636,6 +636,27 @@ class Forge:
                 pass
         return self.match_ids(list(ds_ids)).search()
 
+    def get_dataset_version(self, source_name):
+        """Get the version of a certain dataset
+
+        Arguments:
+        source_name (string): Name of the dataset
+
+        Returns:
+        int: Version of the dataset in question
+        """
+
+        hits = self.search("mdf.source_name:{}_v* AND"
+                           " mdf.resource_type:dataset".format(source_name),
+                           advanced=True, limit=2)
+
+        # Some error checking
+        if len(hits) == 0:
+            raise Exception('No such dataset found: ' + source_name)
+        elif len(hits) > 1:
+            raise Exception('Matched >1 dataset. Please contact MDF developers')
+        return hits[0]['mdf']['version']
+
 
 # ***********************************************
 # * Data retrieval functions
